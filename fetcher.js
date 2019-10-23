3598e1607fd4947de3144ba75f8e4e44892e34b4
Matthew Wardle <mwardle.22@gmail.com>
2019-10-23 21:15:19 +0000

    Implement fetcher function which retrieves and saves HTML data for a given URL

--- fetcher.js
+++ fetcher.js
@@ -1,11 +1,38 @@
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 let args = process.argv;
 
-const requestSite = request(url, (error, response, body) => {
-  return body;
-})
 
 const fetcher = function(url, filePath) {
-  let siteData = requestSite(url (error, response, body));
-}
+  request(url, (error, response, body) => {
+    console.log(error);
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        console.log(`${filePath} does not exist`);
+        fs.writeFile(filePath, body, () => {
+          console.log(`Downloaded and saved to ${filePath}`);
+        });
+        rl.close();
+      } else if (!err) {
+        console.log(`${filePath} exists`);
+        rl.question("File already exists. Overwrite file? (Y/N)", (answer) => {
+          if (answer === "\u004e") { //N
+            process.exit();
+          } if (answer === "\u0059") { //Y
+            rl.close();
+            fs.writeFile(filePath, body, () => {
+              console.log(`Downloaded and saved to ${filePath}`);
+            });
+          }
+        });
+      }
+    });
+  });
+};
+
+fetcher(args[2], args[3]);
